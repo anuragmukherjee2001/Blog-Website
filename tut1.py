2c64bb9b45b426f08fc361c4f24aff098348767a
Anurag <anuragmukherjee2001@gmail.com>
2020-10-14 12:37:54 +0530

    Initial commit 14/10/2020

---
+++ tut1.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+@app.route("/about")
+def anurag():
+    name = "Anurag"
+    return render_template('about.html', name2 = name)
+app.run(debug=True)
